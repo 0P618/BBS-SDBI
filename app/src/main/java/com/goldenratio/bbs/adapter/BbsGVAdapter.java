8baab49b9522b7417870894be146487fb5e69be2
bfchengnuo <bfchengnuo@gmail.com>
2017-04-15 12:21:51 +0800

    + BBS界面的悬浮按钮等 * 重构依赖

--- app/src/main/java/com/goldenratio/bbs/adapter/BbsGVAdapter.java
+++ app/src/main/java/com/goldenratio/bbs/adapter/BbsGVAdapter.java
@@ -1,32 +1,75 @@
 package com.goldenratio.bbs.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.goldenratio.bbs.R;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 冰封承諾Andy on 2017/4/14 0014.
  * 论坛界面的板块 GV 的适配器
+ * 目前来说，布局比较简单，使用简单适配器也可以
  */
 
 public class BbsGVAdapter extends BaseAdapter {
+    private List<Map<String, Object>> mMapList;
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+
+    public BbsGVAdapter(List<Map<String, Object>> mapList, Context context) {
+        mMapList = mapList;
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return mMapList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mMapList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = mLayoutInflater.inflate(R.layout.grid_item, parent,false);
+            viewHolder.initView(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        // 填充数据
+        viewHolder.mImageView.setImageResource((Integer) mMapList.get(position).get("img"));
+        viewHolder.mTextView.setText((CharSequence) mMapList.get(position).get("text"));
+        return convertView;
+    }
+
+    static class ViewHolder {
+        private ImageView mImageView;
+        private TextView mTextView;
+
+        private void initView(View view) {
+            mImageView = (ImageView) view.findViewById(R.id.img_item);
+            mTextView = (TextView) view.findViewById(R.id.txt_item);
+        }
+
     }
 }
--- app/src/main/java/com/goldenratio/bbs/ui/fragment/BBSFragment.java
+++ app/src/main/java/com/goldenratio/bbs/ui/fragment/BBSFragment.java
@@ -1,32 +1,76 @@
 package com.goldenratio.bbs.ui.fragment;
 
-
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.goldenratio.bbs.R;
+import com.goldenratio.bbs.adapter.BbsGVAdapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class BBSFragment extends Fragment {
+public class BBSFragment extends Fragment implements AdapterView.OnItemClickListener {
     private GridView mGvTitle;
     private ListView mLvShow;
-
+    private int[] img;
+    private String[] imgName;
+    private List<Map<String, Object>> mapList;
+    private FloatingActionButton mFab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recommend, container, false);
+        View view = inflater.inflate(R.layout.fragment_bbs, container, false);
 
         mGvTitle = (GridView) view.findViewById(R.id.gv_title);
         mLvShow = (ListView) view.findViewById(R.id.lv_show);
+
+        initData();
+        mGvTitle.setAdapter(new BbsGVAdapter(mapList, getContext()));
+        mGvTitle.setOnItemClickListener(this);
+
+        mFab = (FloatingActionButton) view.findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         return view;
     }
 
+    private void initData() {
+        mapList = new ArrayList<>();
+        // 图片
+        img = new int[]{R.mipmap.ic_launcher, R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+        // 对应的文字
+        imgName = new String[]{"conan", "death_note", "naruto", "one_piece"};
+
+        for (int i = 0; i < img.length; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("img", img[i]);
+            map.put("text", imgName[i]);
+            mapList.add(map);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "点击的是：" + mapList.get(position).get("text"), Toast.LENGTH_SHORT).show();
+    }
 
 
 }
